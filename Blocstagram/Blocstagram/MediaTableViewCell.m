afb7f9ca2c50e0dbe8d7dfe1057d0ca7d63597e4
Matt Krueger <mpkrueger@gmail.com>
2015-07-09 20:19:16 -0700

    Changed the default value for image height when an image isn't present to reduce jumpiness and complete assignment for checkpoint 38

--- Blocstagram/Blocstagram/MediaTableViewCell.m
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -184,7 +184,7 @@ static NSParagraphStyle *paragraphStyle;
     if (self.mediaItem.image.size.width > 0 && CGRectGetWidth(self.contentView.bounds)) {
         self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
     } else {
-        self.imageHeightConstraint.constant = 0;
+        self.imageHeightConstraint.constant = 150;
     }
     
     self.separatorInset = UIEdgeInsetsMake(0, CGRectGetWidth(self.bounds)/2.0, 0, CGRectGetWidth(self.bounds)/2.0);
