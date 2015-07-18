e683dd0d816e91a4a74365c294345c0e7f3041d2
Matt Krueger <mpkrueger@gmail.com>
2015-07-17 23:30:50 -0700

    Completed checkpoint 43, added filters

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Matt Krueger on 7/17/15.
+//  Copyright (c) 2015 Matt Krueger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
